def3d2f7dd8772c60fcdf7aa9ac1f2dbe607a8f4
erikrecio <erikrecio@gmail.com>
2022-08-17 22:24:28 +0200

    hey jude

--- plots.py
+++ plots.py
@@ -1,94 +1,97 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-fig, axs = plt.subplots(ncols=2, nrows=2, figsize=(8, 6))
+for plt_nsites in [4, 6, 8]:
+    for plt_nqubits0 in range(1, plt_nsites):
 
-ax = axs[0,0]
+        with open('data.tsv') as f:
+            lines = f.readlines()
 
-x = np.linspace(2,5,10)
-y = x**2 + x + 1 + np.random.rand(len(x)) # some random data
+            x1 = []
+            x2 = []
+            x3 = []
+            x4 = []
+            
+            y1 = []
+            y2 = []
+            y3 = []
+            y4 = []
 
-z = np.linspace(2,5,10)
-t = z**2 + z + 1 + np.random.rand(len(z)) + 2*z # some random data
+            for line in lines:
 
-ax.set_title("title")
-ax.plot(x, y , label="plot1")
-ax.plot(z, t , label="plot2")
-ax.legend(fontsize=8)
-ax.set_xlabel("n_wires")#, fontsize=16)
-ax.set_ylabel("time (s)")#, fontsize=16)
-ax.set_yscale("linear")
-ax.set_xscale("linear")
+                min_loss = float(line.split()[1])
+                nsites = int(line.split()[2])
+                nqubits0 = int(line.split()[3])
+                h = float(line.split()[4])
+                nlayers = int(line.split()[5])
+                entropy = float(line.split()[6])
 
-ax = axs[1,0]
+                if nsites == plt_nsites:
+                    if nqubits0 == plt_nqubits0:
 
-ax.set_title("title")
-ax.plot(x, y , "x--", label="legend description")
-ax.legend(fontsize=8)
-ax.set_xlabel("nx")#, fontsize=16)
-ax.set_ylabel("y $\\theta$ also latex no problem")#, fontsize=16)
-ax.set_yscale("log")
-ax.set_xscale("log")
+                        if nlayers == 1:
+                            x1.append(h)
+                            y1.append(min_loss)
 
+                        if nlayers == 2:
+                            x2.append(h)
+                            y2.append(min_loss)
 
-# ax = axs[0,0]
+                        if nlayers == 3:
+                            x3.append(h)
+                            y3.append(min_loss)
 
-# x = np.linspace(2,5,10)
-# y = x**2 + x + 1 + np.random.rand(len(x)) # some random data
-
-# ax.set_title("title")
-# ax.plot(x, y , "x--", label="legend description")
-# ax.legend(fontsize=8)
-# ax.set_xlabel("n_wires")#, fontsize=16)
-# ax.set_ylabel("time (s)")#, fontsize=16)
-# ax.set_yscale("linear")
-# ax.set_xscale("linear")
-
-# ax = axs[1,0]
-
-# ax.set_title("title")
-# ax.plot(x, y , "x--", label="legend description")
-# ax.legend(fontsize=8)
-# ax.set_xlabel("nx")#, fontsize=16)
-# ax.set_ylabel("y $\\theta$ also latex no problem")#, fontsize=16)
-# ax.set_yscale("log")
-# ax.set_xscale("log")
-
-plt.savefig("savefiguresomewhere.png", dpi=200)
-
-#--------------------------------------------------------------------------------------
+                        if nlayers == 4:
+                            x4.append(h)
+                            y4.append(min_loss)
 
 
-# import pandas as pd
-# import os
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import matplotlib.cm as cm
-# from matplotlib import rc
-# from matplotlib import gridspec
-# import cmath
-# import scipy as scipy
-# from mpl_toolkits.mplot3d import Axes3D
-# from matplotlib.colors import ListedColormap
-# from ast import literal_eval    
+            if x1 or x2 or x3 or x4:
+                fig, axs = plt.subplots(ncols=2, nrows=2, sharex = True)
+                fig.suptitle("4 sites, 1 qubit0")
 
-# #plt.style.use('/Users/paolostornati/Phd/phdthesis.mpltstyle')
+                v_labelsize = 7
+                v_fontsize = 8
 
-# niceblack="#262626"
-# from matplotlib.colors import ListedColormap
-# import palettable
-# from palettable.cartocolors.sequential import DarkMint_7
-# cmap = ListedColormap(DarkMint_7.mpl_colors)
-# colors_mint = cmap.colors
-# # from palettable.colorbrewer.sequential import Blues_8
+                ax = axs[0,0]
 
-# cmap = ListedColormap(palettable.cmocean.sequential.Amp_20.mpl_colors)
-# reds = cmap.colors
+                ax.scatter(x1, y1, s=5, marker='o', label="1 layer")
+                ax.set_ylabel("min_loss")
+                ax.ticklabel_format(axis='y', style='sci', scilimits=(0,0))
+                ax.legend(fontsize=v_fontsize)
+                ax.tick_params(labelsize=v_labelsize)
+                ax.yaxis.get_offset_text().set_fontsize(v_fontsize)
 
-# cmap = ListedColormap(palettable.cmocean.diverging.Balance_14.mpl_colors)
-# balance14 = cmap.colors
 
-# cmap = ListedColormap(palettable.matplotlib.Inferno_20.mpl_colors)
-# inferno = cmap.colors
+                ax = axs[0,1]
 
-# plt.savefig("savefiguresomewhere2.png", dpi=200)
+                ax.scatter(x2, y2, s=5, marker="o", label="2 layers")
+                ax.ticklabel_format(axis='y', style='sci', scilimits=(0,0))
+                ax.legend(fontsize=v_fontsize)
+                ax.tick_params(labelsize=v_labelsize)
+                ax.yaxis.get_offset_text().set_fontsize(v_fontsize)
+
+
+                ax = axs[1,0]
+
+                ax.scatter(x3, y3, s=5, marker="o", label="3 layers")
+                ax.set_xlabel("h")
+                ax.set_ylabel("min_loss")
+                ax.ticklabel_format(axis='y', style='sci', scilimits=(0,0))
+                ax.legend(fontsize=v_fontsize)
+                ax.tick_params(labelsize=v_labelsize)
+                ax.yaxis.get_offset_text().set_fontsize(v_fontsize)
+
+
+                ax = axs[1,1]
+
+                ax.scatter(x4, y4, s=5, marker="o", label="4 layers")
+                ax.set_xlabel("h")
+                ax.ticklabel_format(axis='y', style='sci', scilimits=(0,0))
+                ax.legend(fontsize=v_fontsize)
+                ax.tick_params(labelsize=v_labelsize)
+                ax.yaxis.get_offset_text().set_fontsize(v_fontsize)
+
+                figure_name = "nsites = " + str(plt_nsites) + ", nqubits0 = " + str(plt_nqubits0) + ".png"
+
+                plt.savefig(figure_name, dpi=200)
